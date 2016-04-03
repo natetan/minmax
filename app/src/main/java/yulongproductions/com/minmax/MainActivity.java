2ae98c6474af4a3fb0970aa6c72268e95742acf3
Yulong Tan <swifties@uw.edu>
2016-04-03 01:24:37 -0700

    added toast messages

--- app/src/main/java/yulongproductions/com/minmax/MainActivity.java
+++ app/src/main/java/yulongproductions/com/minmax/MainActivity.java
@@ -4,14 +4,48 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    private Button minMax;
+    private TextView display;
+    private Nums mNums;
+
+    public static final int RANGE = 1000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        minMax = (Button) findViewById(R.id.minMaxButton);
+        display = (TextView) findViewById(R.id.display);
+        display.setText((int) Math.random() * RANGE);
+        final int displayNum = Integer.parseInt(display.getText().toString());
+        mNums = new Nums();
+
+        minMax.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "The min is " + mNums.getMin(displayNum);
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        minMax.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                String message = "The max is " + mNums.getMax(displayNum);
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
     }
 
     @Override
